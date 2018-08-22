c900cb65eeecf76d7cbaec51d32dd5ac13f3ffec
akash-gupta-146 <ag.akash007@gmail.com>
2018-08-22 18:21:06 +0530

    Created home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController} from 'ionic-angular';
+import { GenerateChallanComponent } from '../../components/generate-challan/generate-challan';
+import { FeaturesProvider } from '../../providers/features/features';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  pages: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public features:FeaturesProvider,     
+  ) {
+    this.pages = this.features.appFeatures;
 
+  }
+
+  open(feature:any){
+    this.navCtrl.push(feature.component)
   }
 
 }
