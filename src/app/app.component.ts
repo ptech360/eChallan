7a7115370a97b7f691ed37fd7660a8233dbdb02d
ptech123 <kharetwal.pankaj111@gmail.com>
2018-09-26 18:32:36 +0530

    check token in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit} from '@angular/core';
-import { Platform, Events } from 'ionic-angular';
+import { Platform, Events} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginComponent } from '../components/login/login';
+import { User } from '../providers/user/user';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp implements OnInit{
-  rootPage:any = LoginComponent;
+  rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public events:Events) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public events:Events, public user: User) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,11 +24,16 @@ export class MyApp implements OnInit{
   
   ngOnInit(){
     this.handleEvent();
+    if(this.user.isLoggedIn()){
+      this.rootPage = TabsPage;
+    }else {
+      this.rootPage = LoginComponent;
+    }
   }
 
   handleEvent(){
     this.events.subscribe('session:expired', () => {
       this.rootPage = LoginComponent;
-    });
+    }); 
   }
 }
